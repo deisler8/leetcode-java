16d91443bf2710d4f4c97d5e857832ffe3ab393e
lilu3 <lilu3@cisco.com>
2015-08-17 09:53:15 -0400

    array q1 q2

---
+++ array/1.Remove_Duplicates_from_Sorted_Array/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length <= 1) {
+            return nums.length;
+        }
+        int index = 1;
+        for(int i =1; i < nums.length; i++) {
+            if(nums[i] != nums[index - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
